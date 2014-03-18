b5b8c982282569605e648f31f6abdbf93c44e53c
Simon Leblanc <contact@leblanc-simon.eu>
2014-03-18 02:33:58 +0100

    Gestion de la planification de l'envoi des recus fiscaux

--- lib/Gesdon/App/TaskRecu.php
+++ lib/Gesdon/App/TaskRecu.php
@@ -14,31 +14,40 @@ class TaskRecu extends Main
         $begin->setTime(0, 0, 0);
 
         $end = new \DateTime();
-        $begin->setDate($begin->format('Y') - 1, 12, 31);
-        $begin->setTime(0, 0, 0);
+        $end->setDate($end->format('Y') - 1, 12, 31);
+        $end->setTime(0, 0, 0);
 
         return $this->render(array('begin' => $begin, 'end' => $end));
     }
 
     public function executePost()
     {
-        $begin = trim((string)$this->request->get('begin'));
-        $end = trim((string)$this->request->get('end'));
+        try {
+            $begin = trim((string)$this->request->get('begin'));
+            $end = trim((string)$this->request->get('end'));
 
-        $params = array('debut' => $begin, 'fin' => $end);
+            $params = array('debut' => $begin, 'fin' => $end);
 
-        foreach ($params as $key => $value) {
-            if (preg_match('#[0-9]{4}-[0-9]{2}-[0-9]{2}#', $value) === 0) {
-                if (preg_match('#(?<day>[0-9]{2})/(?<month>[0-9]{2})/(?<year>[0-9]{4})#', $value, $matches) === 1) {
-                    $params[$key] = $matches['year'].'-'.$matches['month'].'-'.$matches['day'];
-                } else {
-                    throw new Exception($key.' must be a date');
+            foreach ($params as $key => $value) {
+                if (preg_match('#[0-9]{4}-[0-9]{2}-[0-9]{2}#', $value) === 0) {
+                    if (preg_match('#(?<day>[0-9]{2})/(?<month>[0-9]{2})/(?<year>[0-9]{4})#', $value, $matches) === 1) {
+                        $params[$key] = $matches['year'].'-'.$matches['month'].'-'.$matches['day'];
+                    } else {
+                        throw new Exception($key.' must be a date');
+                    }
                 }
             }
+
+            TaskManagerPeer::add('send:recus', $params);
+
+            // Add success message
+            $this->session->getFlashBag()->add('notice', 'L\'envoi des reçus fiscaux a été programmé.');
+        } catch (\Exception $e) {
+            // Add error message
+            $this->session->getFlashBag()->add('error', 'L\'envoi des reçus fiscaux n\'a pas été programmé : '.$e->getMessage());
+            return $this->getApp()->redirect($this->url->generate('task_recus'));
         }
 
-        $manager = TaskManagerPeer::add('send:recus', $params);
-
-        return $this->render();
+        return $this->getApp()->redirect($this->url->generate('task'));
     }
 }
